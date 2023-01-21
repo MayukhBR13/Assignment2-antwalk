3f892fcb79e064499a02129dad558df407555821
MayukhBR13 <mayukhbarmanray3@gmail.com>
2023-01-22 03:48:31 +0530

    first commit

---
+++ ForEach.java
@@ -0,0 +1,14 @@
+class ForEach
+{
+   public static void main(String args[]) 
+    {   
+       
+      float[] ar={10f,2.7f,89.7f}; 
+      float sum=0;
+      for(float a:ar)
+	sum+=a;
+       
+          
+      System.out.println("Sum: " + sum);      
+   }
+}
